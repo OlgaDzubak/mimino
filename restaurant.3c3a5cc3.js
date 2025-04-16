7611c0fa814eda8eb85e185feef71a7797020008
OlgaDzubak <118764742+OlgaDzubak@users.noreply.github.com>
2025-04-16 16:27:07 +0000

    Deploying to gh-pages from @ OlgaDzubak/mimino@b6936e555ac3c1f30e5c095edf598a9f915d47c5 🚀

---
+++ restaurant.3c3a5cc3.js
@@ -0,0 +1,2 @@
+var showMenuBtn=document.querySelector(".js-show-menu"),goToMenu=function(){window.scrollTo({top:document.querySelector(".restaurant-menu-section").offsetTop-75,behavior:"smooth"})};showMenuBtn.addEventListener("click",goToMenu);
+//# sourceMappingURL=restaurant.3c3a5cc3.js.map
