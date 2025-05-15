2e0c0f54fb54f503fda8a18bcbd97f1644bb4f1f
Olga Dzubak <iamAndriy80@gmail.com>
2025-05-15 17:32:45 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 
+//Функції відправки на сервер бронювання номеру або столика (для найбутнього бекенду)
+
 export class mimino_API {
 
   #BASE_URL = 'https://mimino-server-4bkr.onrender.com/';
